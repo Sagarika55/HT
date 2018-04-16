799f10e5050549bf2b939a9359be71ef79e491c4
Soumendra Hati <soumendrahati@gmail.com>
2018-04-16 22:23:44 +0530

    Service Layer Added

---
+++ SH.HMAngular/Controllers/CandidateController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SH.HMAngular.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Candidate")]
+    public class CandidateController : Controller
+    {
+        // GET: api/Candidate
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Candidate/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Candidate
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+        
+        // PUT: api/Candidate/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SH.HMAngular/Services/DashboardService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SH.HMAngular.Models;
+
+namespace SH.HMAngular.Services
+{
+    public class DashboardService : IDashboardService
+    {
+        public List<Candidate> GetCandidate()
+        {
+            IList<Candidate> candidates = new List<Candidate>
+            {
+                new Candidate { CandidateId = 1, CandidateName = "Soumendra", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 2, CandidateName = "Asim", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 3, CandidateName = "Anil", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 4, CandidateName = "Abhinav", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 5, CandidateName = "Asutosasis", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 6, CandidateName = "Sakti", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 7, CandidateName = "Vidharva", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 8, CandidateName = "Rakesh", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 9, CandidateName = "Abhijit", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 10, CandidateName = "Om", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 11, CandidateName = "Kunal", Phone = "9999999999", CandidateStatus =  CandidateStatus.L3},
+                new Candidate { CandidateId = 12, CandidateName = "Soumendra", Phone = "9999999999", CandidateStatus =  CandidateStatus.L2},
+                new Candidate { CandidateId = 13, CandidateName = "Om", Phone = "9999999999", CandidateStatus =  CandidateStatus.L2},
+                new Candidate { CandidateId = 14, CandidateName = "Vidharva", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 15, CandidateName = "Pravat", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 16, CandidateName = "Vidharva", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 17, CandidateName = "Vidharva", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 18, CandidateName = "Vidharva", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 19, CandidateName = "Asutosasis", Phone = "9999999999", CandidateStatus =  CandidateStatus.L1},
+                new Candidate { CandidateId = 20, CandidateName = "Anil", Phone = "9999999999", CandidateStatus =  CandidateStatus.L2}
+            };
+
+            return candidates.ToList();
+        }
+    }
+}
